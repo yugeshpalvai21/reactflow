9687ae713b78ad06a63a27dd3d9abaf152e1dcd2
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-20 08:25:29 +0530

    adds react and react-dom and imported in webpack way

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-const isAdult = require("./utils.js");
-import { isSenior } from './isSenior.js';
-// const isSenior = require("./isSenior.js");
+import React from 'react';
+import ReactDOM from 'react-dom';
+// const React = require('react')
+console.log(typeof React);
+console.log(typeof ReactDOM);
+console.dir(React)
 
-console.log(isAdult(18));
+// const isAdult = require("./utils.js");
+// import { isSenior } from './isSenior.js';
+// // const isSenior = require("./isSenior.js");
 
-console.log(isSenior(18));
+// console.log(isAdult(18));
+
+// console.log(isSenior(18));
 
 
