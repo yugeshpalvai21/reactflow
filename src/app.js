19f6c47544ddcc716568e4009effbb9f4c246370
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-22 09:29:29 +0530

    implementes functional component in es6 way

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@ import './styles/main.scss';
 
 const domElement = document.getElementById('main');
 
-const Header = function() {
-    return (
+const Header = (props) => (
         <div>
             <h1>This is functional component</h1>
+            <p>Name: { props.name }</p>
         </div>
     )
-}
 
-ReactDOM.render(<Header />, domElement);
+ReactDOM.render(<Header name="yugesh" />, domElement);
