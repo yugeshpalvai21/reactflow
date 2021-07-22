b5dca7b197679fcc5fd5a434a153fc2c8c441c90
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-22 08:57:58 +0530

    implements JSX expressions

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const jsx = (
     <div>
         <h1>Hello World</h1>
         <p>This Para Transpiled from JSX</p>
+        <p>Addition of 3,5 is { 3 + 5 }</p>
+        { true && <p>This para Generated if jsx expression returns true</p>}
+        {[<li>one</li>, <li>two</li>, <li>three</li>]}
+        {[1, 'one', true]}
     </div>
 );
 
