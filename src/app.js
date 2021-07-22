587ef81158063e4aeb9d7dbd0e74f958f28bb9aa
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-22 09:15:18 +0530

    implements functional component - es5 way

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
-import { divide } from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
 
 const domElement = document.getElementById('main');
 
-const jsx = (
-    <div>
-        <h1>Hello World</h1>
-        <p>This Para Transpiled from JSX</p>
-        <p>Addition of 3,5 is { 3 + 5 }</p>
-        { true && <p>This para Generated if jsx expression returns true</p>}
-        {[<li>one</li>, <li>two</li>, <li>three</li>]}
-        {[1, 'one', true]}
-    </div>
-);
+const Header = function() {
+    return (
+        <div>
+            <h1>This is functional component</h1>
+        </div>
+    )
+}
 
-ReactDOM.render(jsx, domElement);
+ReactDOM.render(<Header />, domElement);
