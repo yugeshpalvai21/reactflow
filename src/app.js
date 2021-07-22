cc33744332fdf0f461490830fd3bf23335cccd30
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-22 09:40:45 +0530

    implements class component with state defination

--- src/app.js
+++ src/app.js
@@ -4,11 +4,20 @@ import './styles/main.scss';
 
 const domElement = document.getElementById('main');
 
-const Header = (props) => (
-        <div>
-            <h1>This is functional component</h1>
-            <p>Name: { props.name }</p>
-        </div>
-    )
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { location: 'IND'};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Name: { this.props.name }</h1>
+                <p>Location: { this.state.location }</p>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<Header name="yugesh" />, domElement);
