5c1950971a51cf9503f1f30a4b662921f7b56fe9
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-22 08:45:00 +0530

    Creating React Element in JSX way

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
+import { divide } from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
 
 const domElement = document.getElementById('main');
 
-const jsx = React.createElement('h2', {}, 'Hello World');
+const jsx = (
+    <div>
+        <h1>Hello World</h1>
+        <p>This Para Transpiled from JSX</p>
+    </div>
+);
 
 ReactDOM.render(jsx, domElement);
