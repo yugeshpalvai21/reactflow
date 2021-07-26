c4340443d6a1bf6978df41b877019fc0c5ef0043
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-07-26 08:09:31 +0530

    UnChecked App - adds header component

--- src/app.jsx
+++ src/app.jsx
@@ -4,15 +4,20 @@ import './styles/main.scss';
 
 const domElement = document.getElementById('main');
 
+const Header = (props) => (
+    <div>
+        <h1>UnChecked App</h1>
+        <p>Manage Dails Updates</p>
+    </div>
+)
 class UnChecked extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
             <div>
-                <div>Header Component</div>
+                <Header />
                 <div>ToDos Component</div>
                 <div>Form Component</div>
             </div>
